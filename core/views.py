4ac0fe374e411b3f51178e00e9b6512cea583e5c
Rajan Gupta <rajanguptag52@gmail.com>
2021-06-30 05:14:09 -0700

    commit Authentication

--- core/views.py
+++ core/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
 def HomePageView(request):
     return render(request, 'HomePageView.html')
 
+@login_required()
 def CustomerPageView(request):
     return render(request, 'HomePageView.html')
+
+@login_required()
 def CourierPageView(request):
     return render(request, 'HomePageView.html')
+
+def SignUpPageView(request):
+    form = forms.SignUpForm()
+    if request.method =='POST':
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email').lower()
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+            login(request, user)
+            return redirect('/')
+    return render(request, 'sign_up.html',{'form':form})
